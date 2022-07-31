58e78f9cba44132359551fd38fba44c82b57a343
TahminaM <Tahminamunni07@gmail.com>
2022-07-30 21:42:58 -0400

    Added Project 1

--- Project: 1/script.js
+++ Project: 1/script.js
@@ -31,13 +31,13 @@ $(document).ready(function(){
     });
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["SWE Student", "Developer", "Programmer", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["SWE Student", "Developer", "Programmer", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true

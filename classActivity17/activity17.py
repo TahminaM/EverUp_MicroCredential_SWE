9a1035a90c2df5aa257f784fabdb0d70648d90e4
TahminaM <Tahminamunni07@gmail.com>
2022-08-04 00:46:02 -0400

    Added activity 17

--- classActivity17/activity17.py
+++ classActivity17/activity17.py
@@ -1,2 +1,38 @@
-print("Hello world! from Pyhton.")
-#variables
+# Tahmina A Munni: Activity 17
+
+
+print("Hello World from Python!")
+
+# creating variables
+number1 = -30
+number2 = 15.6
+addiition = number1 + number2
+name = "Tahmina Munni"
+character = 'T'
+
+
+# Boolean for later
+no_yes = False
+print("Number 1 = ", number1)
+
+
+# Input() Function
+firstName = input("Enter a name: ")
+print("Welcome to the program: ", firstName)
+
+
+# casting from string to integer
+num1 = input("Enter a number: ")
+num1 = int(num1)
+num2 = int(input("Enter a 2nd number: "))
+prod = num1*num2
+print("The product of ", num1, " and ", num2, " is ", prod)
+
+
+#ex 1: find the hypotenuse of a right angle
+h = float(input("Enter the height: "))
+w = float(input("Enter the width: "))
+hyp = (h**2+w**2)**0.5
+print("A hypotenuse is: ", hyp)
+
+

8ec2cb0e99a3fe7f6969d587573638ed71c6dff4
TahminaM <Tahminamunni07@gmail.com>
2022-08-04 01:23:32 -0400

    added 18

--- classActivity18/activity18.py
+++ classActivity18/activity18.py
@@ -23,3 +23,90 @@ elif age == 7:
     print("Age = 7. Height should be around 113cm and weigth 18.0lbs")
 else:
     print("Unable to display height and weigth")
+
+
+
+
+#ex 3: and + or operators
+age = int(input("Enter an age: "))
+gender = input("Type M for male or F for female: ")
+if age == 5 and gender == "M" or gender == "m":
+    print("5 year old boy")
+elif age == 5 and gender == "F" or gender == "f":
+    print("5 year old girl!")
+else:
+    print("Any other ages other than 5")
+
+
+
+# For loop:  ex 4: for loop basics   
+print("\n\n------------------ FOR LOOP ------------------")
+for x in range(1,5):
+    print("Counting: ", x)
+
+
+
+#Example 5: For loop with three arguments"
+print("Example 5: For loop with three arguments")
+for y in range(2, 30, 3):
+    print("y = ", y)
+
+
+
+#ex 6: for loop decrement count
+for z in range(20,-10,-5):
+    print("z =  ", z)
+
+
+
+#ex 7: for loop in a list
+print("Example 7: For loop in a list")
+colors = ['yellow', 'red', 'blue', 'green', 'white', 'black']
+for c in colors:
+    print("color = ", c)
+
+
+
+#ex 8: for loop in a string
+msg = "Hello World"
+for m in msg:
+    print("characters = ", m)   
+
+
+    
+#Example 9: Nesting for loop and if statement
+for counter in range(10):
+    if counter == 5:
+        continue #break
+    print("Now counting: ", counter)
+    print(" =*=*=*=*=*=*=*=*=*=*=*=*")
+
+
+
+#ex 10: for loop to print num from 10 to -5 and skip nums that are multiple of 4
+for num in range(10, -5, -1):
+    if num%4 == 0:
+        continue
+    print(num)
+
+
+
+#ex 11: for else statement
+for n in range(7):
+    print(n)
+else:
+    print("Done!!")
+
+
+
+#ex 12: while loop basics
+print("\n\n------------------ While LOOP ------------------")
+i = 0
+while i < 6:
+    print("i = ", i)
+    i += 1
+else:
+    print("i is no longer less than 6")
+
+
+

42d980aa89a77e656998a8e2731d5e1147b088a6
TahminaM <Tahminamunni07@gmail.com>
2022-08-07 11:59:55 -0400

    Added Activity 20

--- classActivity20/activity20.py
+++ classActivity20/activity20.py
@@ -3,38 +3,51 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:123@localhost/demoDB'
+app.config ['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:1234@localhost/demoDB'
 db = SQLAlchemy(app)
+
+
+
 
 # Define db model. Create a table in our demoDB
 class Data(db.Model):
-    __tablename__ = "data"
-    id=db.Column(db.Integer, primary_key=True)
-    email= db.Column(db.String(120), unique = True)
+     __tablename__ = "data"
+     id=db.Column(db.Integer, primary_key=True)
+     email = db.Column(db.String(120), unique = True)
 
-def __init__(self, email):
-    self.email = email
+     def __init__(self, email):
+         self.email = email
+
+
+
 
 # homepage when application is ran
 @app.route("/")
 def hello():
     return render_template("index.html")
 
+
+
 #handle the homepage when the form is submitted.
 # request.method =="POST" is Collecting the data of the form when the submit button is clicked
-@app.route("/",methods=["POST"])
+
+
+@app.route("/", methods=["POST"])
 def thankyou():
-    if request.method =="POST":
+    if request.method == "POST":
         email = request.form["email_name"]
         print(request.form)
-     # LAST STEP: commit
-data = Data(email)
-db.session.add(data)
-db.session.commit() #execute the data into database
-   return render_template("thankyou.html")
+    # Last Step: Commit and send data to database
+    data = Data(email)
+    db.session.add(data)
+    db.session.commit() # Execute the data into database
+    return render_template("thankyou.html")
+
+
 @app.route("/about")
 def aboutUs():
     return "<h1>About Us</h1>"
-if __name__=="__main__":
-    app.run()
+
+if __name__ =="__main__":
     app.debug=True
+    app.run()
